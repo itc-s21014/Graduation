a70fcda648a6a5bd55b42a005e581ee27422749f
s21014 <s21014@std.it-coollege.ac.jp>
2023-10-02 15:26:27 +0900

    タスク実行画面

---
+++ app/src/main/java/com/example/myapplication/ExecutionActivity.kt
@@ -0,0 +1,31 @@
+package com.example.myapplication
+
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityExecutionBinding
+
+class ExecutionActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityExecutionBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityExecutionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        scheduleScreenTransition(this)
+    }
+
+    private fun scheduleScreenTransition(context: Context) {
+        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(context, EndActivity::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0)
+
+        val triggerTimeMillis = System.currentTimeMillis() + (5 * 60 * 1000) // 5分後に遷移
+
+        alarmManager.set(AlarmManager.RTC_WAKEUP, triggerTimeMillis, pendingIntent)
+    }
+}
