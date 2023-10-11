f362ceab6ebbb914de5b4a3a98f50a437d4131ce
s21014 <s21014@std.it-coollege.ac.jp>
2023-10-11 14:38:42 +0900

    通知

--- app/src/main/java/com/example/myapplication/ExecutionActivity.kt
+++ app/src/main/java/com/example/myapplication/ExecutionActivity.kt
@@ -6,24 +6,30 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.myapplication.databinding.ActivityExecutionBinding
 import android.widget.TextView
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
 class ExecutionActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityExecutionBinding
     private lateinit var timeTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityExecutionBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_execution)
 
         scheduleScreenTransition(this)
 
-        timeTextView = findViewById(R.id.timeTextView)
+        val timeTextView = findViewById<TextView>(R.id.timeTextView)
+
+        val startTime = Date()
+        val endTime = Date(System.currentTimeMillis() + 3600000)
+        val timeFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
+        val startTimeStr = timeFormat.format(startTime)
+        val endTimeStr = timeFormat.format(endTime)
+
+        val timeRangeText = "$startTimeStr - $endTimeStr"
+        timeTextView.text = timeRangeText
 
         displayCurrentTime()
     }
@@ -35,17 +41,15 @@ class ExecutionActivity : AppCompatActivity() {
 
         timeTextView.text = "$currentTime"
     }
-}
-
-
-
 
     private fun scheduleScreenTransition(context: Context) {
         val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
         val intent = Intent(context, EndActivity::class.java)
-        val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        val pendingIntent =
+            PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
 
         val triggerTimeMillis = System.currentTimeMillis() + (5 * 60 * 1000) // 5分後に遷移
 
         alarmManager.set(AlarmManager.RTC_WAKEUP, triggerTimeMillis, pendingIntent)
     }
+}
