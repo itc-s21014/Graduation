1c8513ccde88437c77cf41cb69f50f85584ae84d
s21014 <s21014@std.it-coollege.ac.jp>
2023-10-17 16:47:17 +0900

    タスク設定追加

---
+++ app/src/main/java/com/example/myapplication/Datalist.kt
@@ -0,0 +1,3 @@
+package com.example.myapplication
+
+data class Datalist(var name: String, var contact: String)
--- app/src/main/java/com/example/myapplication/TaskSettingActivity.kt
+++ app/src/main/java/com/example/myapplication/TaskSettingActivity.kt
@@ -1,101 +1,71 @@
 package com.example.myapplication
 
-import android.content.ContentValues
-import android.database.sqlite.SQLiteDatabase
-import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import android.content.Intent
+import android.database.Cursor
 import androidx.appcompat.app.AppCompatActivity
-
+import android.os.Bundle
+import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class TaskSettingActivity : AppCompatActivity() {
-    var addTask: EditText? = null
-    var addTime: EditText? = null
-    var addBtn: Button? = null
-    var updateBtn: Button? = null
-    var deleteBtn: Button? = null
-    var deleteAllBtn: Button? = null
-    var showBtn: Button? = null
-    var disp: TextView? = null
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var button: FloatingActionButton
+    private lateinit var button2: Button
+    private lateinit var button3: Button
+    lateinit var dbh: DBHelper
+    private lateinit var newArray: ArrayList<Datalist>
+    private lateinit var adapter: MyAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task_setting)
 
-        //部品の取得
-        addTask = findViewById<View>(R.id.addTask) as EditText
-        addTime = findViewById<View>(R.id.addTime) as EditText
-        addBtn = findViewById<View>(R.id.addBtn) as Button
-        updateBtn = findViewById<View>(R.id.updateBtn) as Button
-        deleteBtn = findViewById<View>(R.id.deleteBtn) as Button
-        deleteAllBtn = findViewById<View>(R.id.deleteAllBtn) as Button
-        showBtn = findViewById<View>(R.id.showBtn) as Button
-        disp = findViewById<View>(R.id.disp) as TextView
+        recyclerView = findViewById(R.id.recycler)
+        button = findViewById(R.id.floatingActionButton)
+        button2 = findViewById(R.id.button2)
+        button3 = findViewById(R.id.button3)
 
-        //データベースの生成
-        val hlp = MyDbHelper(this)
-        val db: SQLiteDatabase = hlp.getReadableDatabase() //finalをつけるのに注意
-
-        //データを追加
-        addBtn!!.setOnClickListener {
-            val `val` = ContentValues()
-            `val`.put("task", addTask!!.text.toString())
-            `val`.put("time", addTime!!.text.toString())
-            db.insert("tasktable", null, `val`)
+        button.setOnClickListener{
+            intent = Intent(this, TaskSettingActivity2::class.java)
+            startActivity(intent)
         }
 
-        //データの更新
-        updateBtn!!.setOnClickListener { //nameとageを取得
-            val task = addTask!!.text.toString()
-            val time = addTime!!.text.toString()
-
-            //ContentValuesに値を入れる
-            val `val` = ContentValues()
-            `val`.put("time", time)
-
-            //データベースを更新
-            db.update("tasktable", `val`, "task=?", arrayOf(task))
+        button2.setOnClickListener {
+            intent = Intent(this, CharacterActivity::class.java)
+            startActivity(intent)
         }
 
-        //データの削除
-        deleteBtn!!.setOnClickListener { //名前を取得
-            val task = addTask!!.text.toString()
-
-            //ContentValuesに値を入れる
-            val `val` = ContentValues()
-            `val`.put("task", task)
-
-            //データベースを更新
-            db.delete("tasktable", "task=?", arrayOf(task))
+        button3.setOnClickListener {
+            intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
 
-        //データの全削除
-        deleteAllBtn!!.setOnClickListener { db.delete("tasktable", null, null) }
+        dbh = DBHelper(this)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.setHasFixedSize(true)
+        displayuser()
+    }
 
-        //データを表示
-        showBtn!!.setOnClickListener { //TextViewをリセットする
-            disp!!.setText(null)
-            val c = db.query(
-                "tasktable",
-                arrayOf("task", "time"),
-                null,
-                null,
-                null,
-                null,
-                null
-            )
-            var mov = c.moveToFirst()
-            while (mov) {
-                disp!!.append(
-                    """
-                        ${c.getString(0)}: ${c.getInt(1)}
-                        
-                        """.trimIndent()
-                )
-                mov = c.moveToNext()
+    private fun displayuser() {
+        val newcursor: Cursor? = dbh.gettext()
+        newArray = ArrayList<Datalist>()
+        while (newcursor!!.moveToNext()){
+            val uname = newcursor.getString(0)
+            val unumber = newcursor.getString(1)
+            newArray.add(Datalist(uname, unumber))
+        }
+        adapter = MyAdapter(newArray)
+        recyclerView.adapter = adapter
+        adapter.onItemClickListener(object : MyAdapter.onItemClickListener{
+            override fun onItemClick(position: Int) {
+                val intent = Intent(this@TaskSettingActivity, TaskSettingActivity3::class.java)
+                intent.putExtra("name", newArray[position].name)
+                intent.putExtra("phone", newArray[position].contact)
+                startActivity(intent)
             }
-            c.close()
-        }
+        })
     }
 }
